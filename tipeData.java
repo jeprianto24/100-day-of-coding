4113ffbf70e8ad83b1836fe1d13c858e81690d88
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2023-10-09 19:51:25 +0800

    Add files via upload

---
+++ tipeData.java
@@ -0,0 +1,24 @@
+
+package javaapplication1;
+public class tipeData {
+    public static void main(String[] args) {
+        int angka = 10;
+        double pi = 3.14159;
+        boolean benar = true;
+        char huruf = 'A';
+        String nama = "Jepp";
+        float suhu = 25.5f;
+        long populasiDunia = 7800000000L;
+        byte nilai = 120;
+        short jarak = 1000; 
+        System.out.println("Ini integer " +angka);
+        System.out.println("Ini double " +pi);
+        System.out.println("Ini Boolean "+benar);
+        System.out.println("Ini char "+huruf);
+        System.out.println("Ini string "+nama);
+        System.out.println("Ini float "+suhu);
+        System.out.println("Ini long "+ populasiDunia);
+        System.out.println("Ini byte "+ nilai);
+        System.out.println("Ini short "+jarak);
+    }
+}
