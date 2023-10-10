d82cb59b41ad261c58f08116e9982447a3916e2c
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2023-10-10 16:56:36 +0800

    Add files via upload

---
+++ day_2.java
@@ -0,0 +1,18 @@
+package Isc;
+public class day_2 {
+    public static void main(String[] args) {
+        System.out.println("Biodata sederhana");
+        String nama = "Jeprianto";
+        String nim = "D0222316";
+        String kelas = "Informatika A";
+        int angkatan = 2022;
+        int umur = 20;
+        
+        System.out.println("Nama saya adalah "+nama);
+        System.out.println("Nim         : " +nim);
+        System.out.println("Kelas       : "+kelas);
+        System.out.println("Angkatan    : "+angkatan);
+        System.out.println("Umur        : "+umur);
+        
+    }
+}
