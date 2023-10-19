7a777b587d68671fb0713e53895cb7814629652b
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2023-10-19 20:45:01 +0800

    Update day11.java

--- day11.java
+++ day11.java
@@ -3,7 +3,7 @@ public class day11 {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 int hasil = i * j;
-                String itunya = String.format("%1d x %1d = %d", i, j, hasil);
+                String itunya = String.format("%1d x %1d = %1d", i, j, hasil);
                 System.out.print(itunya + "\t");
             }
             System.out.println(); 
