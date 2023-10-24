472109e4e929c7a41afb5e9b142833ac9533efae
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2023-10-24 21:56:37 +0800

    buat method sederhana, panggil melalui method main

---
+++ day16.java
@@ -0,0 +1,13 @@
+
+package javaapplication4;
+
+public class day16 {
+    public static void cetakData(){
+        System.out.println("Nama      : jep");
+        System.out.println("alamat    : linolamoga");
+        System.out.println("pekerjaan : mahasiswa");
+    }
+    public static void main(String[] args) {
+        cetakData();
+    }
+}
